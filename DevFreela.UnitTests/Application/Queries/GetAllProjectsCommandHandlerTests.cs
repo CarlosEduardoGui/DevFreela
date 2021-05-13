549624630427b2838c3ecc858226328bd9b1cd68
Carlos Eduardo Guimaraes Souza <carlos.guimaraes@iterative.com.br>
2021-05-13 18:14:14 -0300

    Retirado classes e códigos antigos; Adicionado a camada de Tests;

---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectsViewModel()
+        {
+            //Arrange
+            var projects = new List<Project>
+            {
+                new Project("nome", ".", 1, 2, 10000),
+                new Project("nome", ".", 1, 2, 10000),
+                new Project("nome", ".", 1, 2, 10000),
+            };
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            //Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+
+            //Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+
+        }
+    }
+}
---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestProjectStartWorks()
+        {
+            var project = new Project("Nome de Teste", "Descricao", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
